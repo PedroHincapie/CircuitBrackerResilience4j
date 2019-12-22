cddf76ec636e40282fd7581cb64e8ee8dac5060c
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2019-12-22 16:03:29 -0500

    Inicio del proyecto PoC Circuit Bracker

---
+++ src/test/java/com/poc/circuitbrackerspring/CircuitbrackerspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.circuitbrackerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitbrackerspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
